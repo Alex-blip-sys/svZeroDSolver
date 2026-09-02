3f5dea734983954a4277f659152545f344137f18
Alex Barr <aebarr@andrew.cmu.edu>
2026-09-02 10:36:38 -0400

    Modify setup_dofs and update_constant in Capacitor
    Updated the setup_dofs method to handle two degrees of freedom and modified the equation in update_constant to include dP_out.

--- src/model/Capacitor.cpp
+++ src/model/Capacitor.cpp
@@ -3,16 +3,16 @@
 #include "Capacitor.h"
 
 void Capacitor::setup_dofs(DOFHandler& dofhandler) {
-  Block::setup_dofs_(dofhandler, 1, {});
+  Block::setup_dofs_(dofhandler, 2, {});
 }
 
 void Capacitor::update_constant(SparseSystem& system,
                                     std::vector<double>& parameters) {
-  double Rpd = parameters[global_param_ids[ParamId::RPD]];
   double Cp = parameters[global_param_ids[ParamId::CP]];
 
-  // Eq 0: Cp*dP_in - Q_in = 0
+  // Eq 0: Cp*(dP_in - dP_out) - Q_in = 0
   system.E.coeffRef(global_eqn_ids[0], global_var_ids[0]) = Cp;
+  system.E.coeffRef(global_eqn_ids[0], global_var_ids[2]) = -Cp;
   system.F.coeffRef(global_eqn_ids[0], global_var_ids[1]) = -1.0;
 
   // Eq 1: Q_in - Q_out = 0
